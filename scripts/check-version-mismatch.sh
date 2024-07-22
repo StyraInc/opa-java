42743688d204d17ff6e7700e8b37c68baa593a29
Charles Daniels <charlesdaniels@users.noreply.github.com>
2024-07-22 16:53:19 -0400

    fix incorrect license info in build.gradle (#63)

--- scripts/check-version-mismatch.sh
+++ scripts/check-version-mismatch.sh
@@ -5,14 +5,24 @@
 
 set -e
 set -u
-set -x
+# set -x
 
 GEN_YAML_VERSION="$(opa eval -f raw -I 'input.java.version' < .speakeasy/gen.yaml)"
 
 RELEASES_MD_VERSION="$(awk '/^### Generated/ {s=1} s==1 && /java v[0-9]/ {s=0; print}' < RELEASES.md  | tail -n1 | cut -d'[' -f 2 | cut -d']' -f 1 | cut -d' ' -f2 | tr -d 'v')"
 
-if [ "$GEN_YAML_VERSION" != "$RELEASES_MD_VERSION" ] ; then
-	printf "ERROR: .speakeasy/gen.yaml (%s) and RELEASES.md (%s) disagree on the current version, creating a release in this state may result in inconsistent versions appearing in Maven and GitHub\n" "$GEN_YAML_VERSION" "$RELEASES_MD_VERSION" 1>&2
+BUILD_GRADLE_VERSION="$(awk '$1 == "version"' < build.gradle | cut -d"'" -f2)"
+
+SDK_VERSION="$(awk '$3 == "sdkVersion"' < src/main/java/com/styra/opa/openapi/SDKConfiguration.java | cut -d'"' -f2)"
+
+ALL_VERSIONS="$(printf '%s\n%s\n%s\n%s\n' "$GEN_YAML_VERSION" "$RELEASES_MD_VERSION" "$BUILD_GRADLE_VERSION" "$SDK_VERSION")"
+
+if [ "$(echo "$ALL_VERSIONS" | sort | uniq | wc -l)" -gt 1 ] ; then
+	echo "ERROR: version mismatch detected! This may result in incorrect GitHub and/or Maven version numbers during release." 1>&2
+	echo ".speakeasy/gen.yaml: $GEN_YAML_VERSION" 1>&2
+	echo "RELEASES.md: $RELEASES_MD_VERSION" 1>&2
+	echo "build.gradle: $BUILD_GRADLE_VERSION" 1>&2
+	echo "src/main/java/com/styra/opa/openapi/SDKConfiguration.java: $SDK_VERSION" 1>&2
 	exit 1
 fi
 
